608f5169dcd6e6c33f9edf4fccd33e7683a37c75
sliptype <sliptype@gmail.com>
2019-01-12 15:46:35 -0600

    Add vuex-reducer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img @click="update('test')" alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Main from './Main.purs'
+import { mapMutations } from 'vuex'
 
 export default {
   name: 'app',
+
   components: {
     HelloWorld
-  }
+  },
+
+  methods: {
+    ...mapMutations([
+      'update'
+    ])
+},
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import { reducers } from 'vuex-reducer'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    value: {
+      name: 'foo'
+    },
+    history: []
+  },
+
+  mutations: reducers({
+    update: (state, name) => ({
+      value: {
+        name
+      },
+      history: [...state.history, state.value]
+    })
+  })
+})
+
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
