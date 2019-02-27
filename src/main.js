ce13e77497e668c2174feefc32e5dbbd5e636d36
sliptype <sliptype@gmail.com>
2019-02-27 11:33:23 -0600

    [WIP] Add redux

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 import createRouter from './router.js'
 import createStore from './store.js'
 
+// This will inject a the store into every component using a mixin
+createStore();
+
 new Vue({
   render: h => h(App),
   router: createRouter(),
-  store: createStore(),
 }).$mount('#app')
--- src/store.js
+++ src/store.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import { reducers as createMutations } from 'vuex-reducer'
-
-import { mapObjectValues, uncurry } from './utils/utils.js'
-import { initialState as state } from './state/Model.purs'
-import reducers from './state/Reducers.purs'
+import store  from './state/Store.purs'
 
 export default function createStore() {
-  Vue.use(Vuex)
-
-  const uncurriedReducers = mapObjectValues(reducers, uncurry)
-  const mutations = createMutations(uncurriedReducers)
-
-  return new Vuex.Store({
-    state,
-    mutations,
-  })
+  Vue.Mixin({
+    data() {
+      return {
+        store,
+      };
+    },
+  });
 }
