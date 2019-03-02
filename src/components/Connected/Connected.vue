c37a76563cac91ceef7240902861bb4133063e78
sliptype <sliptype@gmail.com>
2019-03-02 14:08:51 -0600

    Allow component props to be passed to connected components

--- src/components/Connected/Connected.vue
+++ src/components/Connected/Connected.vue
@@ -1,7 +1,7 @@
 <template>
   <Provider :mapDispatchToProps="mapDispatchToProps" :mapStateToProps="mapStateToProps" :store="store">
     <template v-slot="{ props, actions }">
-      <component :is="component" :props="props" :actions="actions"/>
+      <component :is="component" :props="{ ...props, ...componentProps }" :actions="actions"/>
     </template>
   </Provider>
 </template>
@@ -12,6 +12,7 @@ import Provider from 'vuejs-redux'
 export default {
   props: [
     'component',
+    'componentProps',
     'mapStateToProps',
     'mapDispatchToProps',
     'store',
